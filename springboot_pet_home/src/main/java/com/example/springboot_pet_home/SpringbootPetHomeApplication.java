f5a78e17f0cae6c8d762e8bcccae8ede16b976bf
fatfuture <101321102+fatfuture@users.noreply.github.com>
2023-07-10 16:16:55 +0800

    first commit

---
+++ springboot_pet_home/src/main/java/com/example/springboot_pet_home/SpringbootPetHomeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_pet_home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPetHomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootPetHomeApplication.class, args);
+    }
+
+}
---
+++ springboot_pet_home/src/test/java/com/example/springboot_pet_home/SpringbootPetHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_pet_home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPetHomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
